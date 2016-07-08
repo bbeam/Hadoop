f8a2f0e4e85934acb7f70fd844c768f93137aad9
DMVarunRauthan <varun.rauthan@datametica.com>
2016-07-08 10:29:10 +0530

    alwebmetrics.properties loaded

--- src/alweb/main/bin/Wrapper_CDC_Load_dim_product.sh
+++ src/alweb/main/bin/Wrapper_CDC_Load_dim_product.sh
@@ -7,21 +7,49 @@
 # Author      : Varun Rauthan                                                  #
 ################################################################################
 
+# Check for the input parameter to the shell script. The script only expects source name as the first parameter.
+Show_Usage()
+{
+	echo "invalid argument please pass only one argument "
+    echo "Usage: $0 <Source Name> <Subject Area>"
+    echo "Ex: $0 alweb alwebmetrics"
+    exit 1
+}
+
+if [ $# -ne 2 ] 
+then
+    Show_Usage
+fi
+
+
+# Copy the alweb.properties file from S3 to HDFS and load the properties.
+aws s3 cp s3://al-edh-dm/src/$1/main/conf/$1.properties /var/tmp/
+
+if [ $? -eq 0 ]
+then
+	. /var/tmp/$1.properties
+else
+	echo "Copy of $1.properties file failed from S3."
+	exit 1
+fi
+
+
+aws s3 cp s3://al-edh-dm/src/$1/main/conf/$2.properties /var/tmp/
+
+if [ $? -eq 0 ]
+then
+	. /var/tmp/$2.properties
+else
+	echo "Copy of $2.properties file failed from S3."
+	exit 1
+fi
+
 # Copy the alweb.properties file from S3 to HDFS and load the properties.
 aws s3 cp s3://al-edh-dm/src/alweb/main/conf/CDC_${TABLE_DIM_PRODUCT}.properties /var/tmp/
 
 if [ $? -ne 0 ]
 then
-	echo "Copy of CDC_product.properties file failed from S3."
-	exit 1
-fi
-
-
-aws s3 cp s3://al-edh-dm/src/common/main/bin/util_generate_pig_SCD2.sh /var/tmp/
-
-if [ $? -ne 0 ]
-then
-	echo "Copy of util_generate_pig_SCD2.sh file failed from S3."
+	echo "Copy of CDC_${TABLE_DIM_PRODUCT}.properties file failed from S3."
 	exit 1
 fi
 
