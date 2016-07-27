914312756d037c24a39f30b71c7004d26c1b997a
DMVarunRauthan <varun.rauthan@datametica.com>
2016-07-27 14:04:20 +0530

    Ingestion wrapper added(extract + DQ)

---
+++ src/alweb/main/bin/wrapper_ingestion_sqoop.sh
@@ -0,0 +1,176 @@
+################################################################################
+#                               General Details                                #
+################################################################################
+# Name        : AngiesList                                                     #
+# File        : wrapper_dq_t_sku.sh                                            #
+# Description : This script performs data quality and cleansing on the data,   #
+#				and finally creates a new table with the elements according to #
+#				the incoming schema							 				   #
+################################################################################
+
+
+# Usage descriptor for invalid input arguments to the wrapper
+Show_Usage()
+{
+    	echo "invalid arguments please pass exactly three arguments "
+    	echo "Usage: "$0" <global properties file with path> <path of local properties file with path> <business date(YYYY-MM-DD)>"
+    	exit 1
+}
+
+if [ $# -ne 3 ]
+then
+    	Show_Usage
+fi
+
+# Copy the al-edh-global.properties file from S3 to local and load the properties.
+global_file=`echo "$(basename $1)"`
+aws s3 cp $1 /var/tmp/
+
+if [ $? -eq 0 ]
+then
+        echo "${global_file} file copied successfully from "$1" to "/var/tmp/" "
+else
+   		echo "Copy of ${global_file} failed from $1."
+    	exit 1
+fi
+
+if [ $? -eq 0 ]
+then
+        . /var/tmp/${global_file}
+else
+		echo "Load of ${global_file} failed"
+		exit 1
+fi
+
+# Copy the local properties file from S3 to local and load the properties.
+local_file=`echo "$(basename $2)"`
+if [ $? -eq 0 ]
+then
+		aws s3 cp $2 /var/tmp/
+else
+   		echo "Copy of ${local_file} failed from $1."
+    	exit 1
+fi
+
+if [ $? -eq 0 ]
+then
+        . /var/tmp/${local_file}
+else
+		echo "Load of ${local_file} failed"
+		exit 1
+fi
+
+echo "****************BUSINESS DATE*****************"
+BUS_DATE=$3
+echo "Business Date : $BUS_DATE"
+
+echo "*************SQOOP IMPORT JOB UTILITY*******************"
+echo -e "executing :\nsqoop import --connect $CONNECTION_URL --username $USERNAME --password $PASSWORD --table $TABLE_NAME --target-dir $S3_BUCKET/$DATA_DIRECTORY=$BUS_DATE -m $MAPPER --fields-terminated-by ',' --lines-terminated-by '\\\n'"
+
+sqoop import --connect $CONNECTION_URL --username $USERNAME --password $PASSWORD  --target-dir $S3_BUCKET/$DATA_DIRECTORY=$BUS_DATE --optiona-file param_inc_t_sku.sqp
+
+if [ $? -eq 0 ]
+then
+  		echo "Successfully Ingested the data into s3 directory $S3_BUCKET/$BUCKET_DIRECTORY=$BUS_DATE"
+else
+  		echo " Sqoop Ingestion Job is Failed" >&2
+  		exit 1
+fi
+
+# Hive Metastore refresh for manual partitioned tables.
+if [ $? -eq 0 ]
+then
+  		hive -e "msck repair table alweb_incoming.inc_t_sku"
+else
+  		echo " Sqoop Ingestion Job is Failed" >&2
+  		exit 1
+fi
+
+# Hive Metastore refresh status check
+if [ $? -ne 0 ]
+then
+  		echo "Hive Metastore refreh failed." >&2
+  		exit 1
+fi
+
+echo "*****************DQ PROCESS STARTS*******************"
+JAR_FILE_NAME=$(basename $DQ_GENERATOR_JAR_FILE_PATH)
+INPUT_JSON_FILE_NAME=$(basename $INPUT_JSON_FILE_PATH)
+SCHEMA_FILE_NAME=$(basename $VALIDATION_SCHEMA_FOR_INPUT_JSON)
+OUTPUT_PIG_FILE_NAME=$(basename $OUTPUT_PIG_FILE_PATH)
+JAR_FILE_NAME=$(basename $DQ_GENERATOR_JAR_FILE_PATH)
+
+# JSON Schema copy to hdfs
+aws s3 cp $VALIDATION_SCHEMA_FOR_INPUT_JSON /var/tmp/
+if [ $? -eq 0 ]
+then
+		echo "JSON schema file $SCHEMA_FILE_NAME for validation copied successfully"
+else
+		echo "copy of JSON schema file $SCHEMA_FILE_NAME for validation failed"
+		exit 1
+fi
+
+# JSON copy to hdfs
+aws s3 cp $INPUT_JSON_FILE_PATH /var/tmp/
+if [ $? -eq 0 ]
+then
+		echo "JSON input file $INPUT_JSON_FILE_NAME copied successfully"
+else
+		echo "copy of JSON input file $INPUT_JSON_FILE_NAME failed"
+		exit 1
+fi
+
+# Copy JAR file to hdfs
+aws s3 cp $DQ_GENERATOR_JAR_FILE_PATH /var/tmp/
+if [ $? -eq 0 ]
+then
+		echo "JSON input file $JAR_FILE_NAME copied successfully"
+else
+		echo " copy of JSON input file $JAR_FILE_NAME failed"
+		exit 1
+fi
+
+# Run java jar program for to generate DQ pig script
+java -cp /var/tmp/EDH_JAVA-1.0-jar-with-dependencies.jar com.angieslist.edh.dq.dqgenerator.DQParser  /var/tmp/$INPUT_JSON_FILE_NAME /var/tmp/$SCHEMA_FILE_NAME
+
+if [ $? -eq 0 ]
+then
+  		echo "$OUTPUT_PIG_FILE_NAME for performing DQ activity created successfully"
+else
+  		echo "$OUTPUT_PIG_FILE_NAME generation failed"
+  		exit 1
+fi
+
+# Copy output pig file to s3
+aws s3 cp $OUTPUT_PIG_FILE_NAME $OUTPUT_PIG_FILE_PATH
+if [ $? -eq 0 ]
+then
+  		echo "$OUTPUT_PIG_FILE_NAME copied to s3"
+else
+  		echo "copying $OUTPUT_PIG_FILE_NAME to s3 failed"
+  		exit 1
+fi
+
+# Pig Script to be triggered for data checking and cleansing.
+pig \
+	-param BUSDATE=$BUS_DATE \
+	-param_file /var/tmp/$global_file \
+	-param_file /var/tmp/$local_file \
+	-file $OUTPUT_PIG_FILE_PATH \
+	-useHCatalog
+
+if [ $? -eq 0 ]
+then
+		echo "$OUTPUT_PIG_FILE_NAME executed without any error."
+else
+		echo "$OUTPUT_PIG_FILE_NAME execution failed."
+		exit 1
+fi
+
+# Hive script to insert audit record
+hive -f $INCOMING_AUDIT_HQL_PATH 
+		-hivevar ENTITY_NAME=$SOURCE_ALWEB 
+		-hivevar INCOMING_DB=$ALWEB_INCOMING_DB 
+		-hivevar INCOMING_TABLE=$TABLE_INC_T_SKU 
+		-hivevar USER_NAME=$USER_NAME 
+		-hivevar BUS_DATE=$BUS_DATE
