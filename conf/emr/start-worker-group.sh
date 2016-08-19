d3c7c2d457fa642001f6ba46ff9d5dacf182c192
Zack Harling <zharling@gmail.com>
2016-08-19 12:18:34 -0400

    Initial commit of sandbox-dm

--- conf/emr/start-worker-group.sh
+++ conf/emr/start-worker-group.sh
@@ -1,2 +1,2 @@
-a=`aws s3 cp  s3://al-edh-stage/conf/emr/task-runner /home/hadoop/TaskRunner --recursive`
-java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar  --workerGroup=ALWorkerGroupStage --region=us-east-1 --logUri=s3://al-edh-stage/logs/emr/workergroup
+a=`aws s3 cp  s3://al-edh-sandbox-dm/conf/emr/task-runner /home/hadoop/TaskRunner --recursive`
+nohup java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar --workerGroup=ALWorkerGroupSandbox-DM --region=us-east-1 --logUri=s3://al-edh-sandbox-dm/logs/emr/workergroup &
