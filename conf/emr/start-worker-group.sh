25051ed58853cb311b194607c9d226de0aeea589
Zack Harling <zharling@gmail.com>
2016-08-19 12:23:12 -0400

    Initial commit of prod

--- conf/emr/start-worker-group.sh
+++ conf/emr/start-worker-group.sh
@@ -1,2 +1,2 @@
-a=`aws s3 cp  s3://al-edh-sandbox-eng/conf/emr/task-runner /home/hadoop/TaskRunner --recursive`
-nohup java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar --workerGroup=ALWorkerGroupSandbox-ENG --region=us-east-1 --logUri=s3://al-edh-sandbox-eng/logs/emr/workergroup &
+a=`aws s3 cp  s3://al-edh-prod/conf/emr/task-runner /home/hadoop/TaskRunner --recursive`
+nohup java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar --workerGroup=ALWorkerGroupProd --region=us-east-1 --logUri=s3://al-edh-prod/logs/emr/workergroup &
