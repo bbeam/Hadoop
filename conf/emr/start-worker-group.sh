54cbd19dae6df760d8c9ae285392d3e2edc944ed
Zack Harling <zharling@gmail.com>
2016-08-19 10:57:38 -0400

    Initial commit for stage env

--- conf/emr/start-worker-group.sh
+++ conf/emr/start-worker-group.sh
@@ -1,2 +1,2 @@
-a=`aws s3 cp  s3://al-edh-uat/conf/emr/task-runner /home/hadoop/TaskRunner --recursive`
-nohup java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar --workerGroup=ALWorkerGroupUAT --region=us-east-1 --logUri=s3://al-edh-uat/logs/emr/workergroup &
+a=`aws s3 cp  s3://al-edh-stage/conf/emr/task-runner /home/hadoop/TaskRunner --recursive`
+java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar  --workerGroup=ALWorkerGroupStage --region=us-east-1 --logUri=s3://al-edh-stage/logs/emr/workergroup
