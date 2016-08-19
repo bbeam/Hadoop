c0b1c541f35555112fab9e2ce8ffd36b117ade5d
Zack Harling <zharling@gmail.com>
2016-08-19 12:51:44 -0400

    Updated worker script with nohup

--- conf/emr/start-worker-group.sh
+++ conf/emr/start-worker-group.sh
@@ -1,2 +1,2 @@
 a=`aws s3 cp  s3://al-edh-stage/conf/emr/task-runner /home/hadoop/TaskRunner --recursive`
-java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar  --workerGroup=ALWorkerGroupStage --region=us-east-1 --logUri=s3://al-edh-stage/logs/emr/workergroup
+nohup java -jar /home/hadoop/TaskRunner/TaskRunner-1.0.jar --workerGroup=ALWorkerGroupstage --region=us-east-1 --logUri=s3://al-edh-stage/logs/emr/workergroup &
