b145183088310aae3030deef1f57d72056694270
abhijeet.purwar <abhijeet.purwar@datametica.com>
2016-08-23 20:15:50 +0530

    market and category ingestion

--- src/common/main/bin/wrapper_dim_load.sh
+++ src/common/main/bin/wrapper_dim_load.sh
@@ -102,10 +102,31 @@ then
 else
     echo "hive execution failed. Dimension table load in target (gold area) process terminated"
     exit 1
+fi
+
+#========Updating maximum surrogate key in the ops_common.surrogate_key_map table========
+
+#hive -e SET hive.exec.dynamic.partition.mode=non-strict; \
+#INSERT OVERWRITE TABLE $OPERATIONS_COMMON_DB.surrogate_key_map PARTITION (table_name) \
+#        SELECT MAX($SURROGATE_KEY) AS $SURROGATE_KEY, '${hivevar:TRGT_DIM_TABLE_NAME}' FROM ${hivevar:GOLD_SHARED_DIM_DB}.${hivevar:TRGT_DIM_TABLE_NAME};
+
+hive -f $UPDATE_SURROGATE_KEY_HQL \
+    -hivevar OPERATIONS_COMMON_DB=$OPERATIONS_COMMON_DB \
+    -hivevar SURROGATE_KEY=$SURROGATE_KEY \
+    -hivevar TRGT_DIM_TABLE_NAME=$TRGT_DIM_TABLE_NAME \
+    -hivevar GOLD_SHARED_DIM_DB=$GOLD_SHARED_DIM_DB
+
+if [ $? -eq 0 ]
+then
+        echo "Max surrogate key updated"
+else
+        echo "Max surrogate key updation failed."
+        exit 1
+fi
 
  LOAD_DIM_AUDIT_HQL_FILE=$(basename $LOAD_DIM_AUDIT_HQL_PATH)
 
- Hive script to insert Dimension load audit record
+# Hive script to insert Dimension load audit record
  hive -f $LOAD_DIM_AUDIT_HQL_PATH \
     -hivevar ENTITY_NAME=$SUBJECT_SHAREDDIM \
     -hivevar OPERATIONS_COMMON_DB=$OPERATIONS_COMMON_DB \
--- src/shareddim/main/hive/load_dim_category.hql
+++ src/shareddim/main/hive/load_dim_category.hql
@@ -1,6 +1,6 @@
 SET hive.exec.dynamic.partition.mode=non-strict;
 -- ######################################################################################################### 
--- HIVE SCRIPT				:targetdim_market_load.hql
+-- HIVE SCRIPT				:targetdim_category_load.hql
 -- AUTHOR					:Hive script is auto generated with java utility.
 -- DESCRIPTION				:Hive script will run after pig script for scd.
 -- #########################################################################################################
@@ -10,9 +10,15 @@ use ${hivevar:GOLD_SHARED_DIM_DB};
 
 -- =========Loading work table to target dimension table========
 INSERT OVERWRITE TABLE ${hivevar:GOLD_SHARED_DIM_DB}.${hivevar:TRGT_DIM_TABLE_NAME}
-SELECT market_key,
-		market_nm,
-		market_id,
+SELECT category_key,
+		category_id,
+		category,
+		legacy_category,
+		new_world_category,
+		additional_category_nm,
+		is_active,
+		category_group,
+		category_group_type,
 		est_load_timestamp,
 		utc_load_timestamp 
  FROM ${hivevar:WORK_SHARED_DIM_DB}.${hivevar:WORK_DIM_TABLE_NAME};
